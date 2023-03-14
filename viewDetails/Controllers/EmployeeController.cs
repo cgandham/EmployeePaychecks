f8a0153fd6b287ff72cd4d7bddfaf322bce0bf21
Chandanasri Gandham <chandanasrigandham@Chandanasris-MacBook-Pro.local>
2023-03-13 23:47:12 -0700

    use url as const

--- viewDetails/Controllers/EmployeeController.cs
+++ viewDetails/Controllers/EmployeeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using viewDetails.Utils;
 
 namespace viewDetails.Controllers
 {
@@ -18,11 +19,19 @@ namespace viewDetails.Controllers
             _httpClient = new HttpClient();
         }
 
+        //[HttpGet(Name = "GetEmployeeDetails")]
+        //public async Task<IActionResult> GetEmployees()
+        //{
+        //    string url = "http://localhost:8080/user/getAll";
+        //    HttpResponseMessage response = await _httpClient.GetAsync(url);
+        //    string responseBody = await response.Content.ReadAsStringAsync();
+        //    return Ok(responseBody);
+        //}
+
         [HttpGet(Name = "GetEmployeeDetails")]
-        public async Task<IActionResult> GetEmployees()
+        public async Task<IActionResult> GetEmployeesPaychecks()
         {
-            string url = "http://localhost:8080/user/getAll";
-            HttpResponseMessage response = await _httpClient.GetAsync(url);
+            HttpResponseMessage response = await _httpClient.GetAsync(Constants.GET_EMPLOYEES_PAYCHECKS_URL);
             string responseBody = await response.Content.ReadAsStringAsync();
             return Ok(responseBody);
         }
--- viewDetails/Utils/Constants.cs
+++ viewDetails/Utils/Constants.cs
@@ -3,7 +3,8 @@ namespace viewDetails.Utils
 {
 	public class Constants
 	{
-		
-	}
+		public const String GET_EMPLOYEES_PAYCHECKS_URL = "http://localhost:8080/user/getAllEmployeePaychecks";
+        public const String GET_EMPLOYEES_URL = "http://localhost:8080/user/getAllEmployeePaychecks";
+    }
 }
 
